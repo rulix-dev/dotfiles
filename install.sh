feb25c1916ce4e781983a765b517cb44c24f7a26
Raúl Pérez Clavero <212502137+rulix-dev@users.noreply.github.com>
2026-04-06 16:10:39 +0200

    Install

--- install.sh
+++ install.sh
@@ -13,3 +13,8 @@ git -C "$RULIX_DEV_HOME/dotfiles" pull
 
 mkdir -p $RULIX_DEV_HOME/dotfiles/target
 cp -fr $RULIX_DEV_HOME/dotfiles/home $RULIX_DEV_HOME/dotfiles/target
+
+BOOTSTRAP_LINE='[[ -s "$HOME/.zshrc.bootstrap" ]] && source "$HOME/.zshrc.bootstrap"'
+if ! grep -qF "$BOOTSTRAP_LINE" "$HOME/.zshrc" 2>/dev/null; then
+    echo "$BOOTSTRAP_LINE" >> "$HOME/.zshrc"
+fi
